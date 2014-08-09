3d7c5ecc276e088451739fd10fb9933813002057
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-09 15:41:18 -0400

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -1046,6 +1046,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16, #??
         DUST_THRESHOLD=0.03e8, #??
     ),
+    saffroncoin=math.Object(
+        P2P_PREFIX='cf0567ea'.decode('hex'),
+        P2P_PORT=19717,
+        ADDRESS_VERSION=63,
+        RPC_PORT=19710,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'saffroncoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: __import__('saffroncoin_subsidy').GetBlockBaseValue(height),
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=30, # s
+        SYMBOL='SFR',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'saffroncoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/saffroncoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.saffroncoin'), 'saffroncoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://188.226.165.184/saffron/block_crawler.php?block_hash=',
+        ADDRESS_EXPLORER_URL_PREFIX='',
+        TX_EXPLORER_URL_PREFIX='http://188.226.165.184/saffron/block_crawler.php?transaction=1',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.03e8,
+    ),
 
 
 )
